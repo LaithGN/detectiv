31a38212c1e3391048f979a5b639aa2d39613abe
Lavri <anutalavrinenko@gmail.com>
2022-04-19 16:32:25 +0400

    fix

--- Himera/scripts/mainMenu/mainMenu.gd
+++ Himera/scripts/mainMenu/mainMenu.gd
@@ -1,17 +1,15 @@
 extends Control
 
+var l="res://Scenes/scenes_one/scene_one.tscn"
+
 
 func ready():
-	for button in $Buttons.get_children():
-		if $Buttons/bt_start_game.pressed(): 
-			$Buttons/bt_start_game.connect("pressed",self,"change_scene",[$Buttons/bt_exit_game.scene_to_open])
-		elif $Buttons/bt_exit_game.pressed():
-			$Buttons/bt_exit_game.connect("pressed",self,"exit")
-		else:
-			get_tree().wait()
-			
-func change_scene(path):
-	SceneChanger.change_scene(path)
-	
-func exit():
-		get_tree().quit()
+	pass
+
+
+func _on_bt_start_game_pressed():
+	SceneChanger.change_scene(l)
+
+
+func _on_bt_exit_game_pressed():
+	get_tree().quit()
