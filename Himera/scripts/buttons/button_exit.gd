913b232c169c0fe86520d90b84b604261d3f2498
Lait <daval2421@gmail.com>
2022-04-19 15:12:58 +0300

    fix mainMenu Buttons control

--- Himera/scripts/buttons/button_exit.gd
+++ Himera/scripts/buttons/button_exit.gd
@@ -1,5 +1,3 @@
 extends Button
 
-
-
 	
--- Himera/scripts/mainMenu/mainMenu.gd
+++ Himera/scripts/mainMenu/mainMenu.gd
@@ -1,9 +1,17 @@
 extends Control
 
 
-func _ready():
+func ready():
 	for button in $Buttons.get_children():
-		button.connect("pressed",self,"change_scene",[button.scene_to_open])
-
+		if $Buttons/bt_start_game.pressed(): 
+			$Buttons/bt_start_game.connect("pressed",self,"change_scene",[$Buttons/bt_exit_game.scene_to_open])
+		elif $Buttons/bt_exit_game.pressed():
+			$Buttons/bt_exit_game.connect("pressed",self,"exit")
+		else:
+			get_tree().wait()
+			
 func change_scene(path):
 	SceneChanger.change_scene(path)
+	
+func exit():
+		get_tree().quit()
